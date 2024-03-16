b946dcd4bb4f4ecadc97e8c8c26041462eba583a
jlcapoterojas <70924972+jlcapor@users.noreply.github.com>
2024-03-15 22:24:05 -0500

    configuracion de login

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>Home Page <Button variant='outline'>Button</Button></div> 
+  );
+}
---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,46 @@
+'use client'
+import { UserButton, useAuth } from "@clerk/nextjs";
+import Container from "../Container";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+import SearchInput from "../SearchInput";
+import { ModeToggle } from "../theme-toggle";
+import { NavMenu } from "./NavMenu";
+const NavBar = () => {
+  const router = useRouter();
+  const {userId} = useAuth()
+  return (
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-1 cursor-pointer" onClick={()=>{router.push("/")}}>
+            <Image
+              src={"/images/logo.png"}
+              alt="Logo"
+              height={30}
+              width={30}
+            />
+            <div className="font-bold text-xl">StaySavvy</div>
+          </div>
+          <SearchInput/>
+          <div className="flex gap-3 items-center">
+            <div>
+              <ModeToggle/>
+              <NavMenu/>
+            </div>
+            <UserButton afterSignOutUrl="/"/>
+            {!userId && 
+              <>
+                <Button onClick={()=> router.push('/sign-in')} variant='outline' size='sm'>Sign in</Button>
+                <Button onClick={()=> router.push('/sign-up')} size='sm'>Sing up</Button>
+              </>
+            }
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default NavBar
